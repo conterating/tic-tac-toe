41bf69eb72a0061d37d3d613881fb7ceadd36da8
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2026-04-24 19:05:46 -0400

    created gameboard object

--- script.js
+++ script.js
@@ -1,90 +1,9 @@
 const gameboard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
 
-  const getBoard = () => {
-    return { board };
+  return {
+    getBoard() {
+      return board;
+    },
   };
-
-  const addMoveToBoard = (move, index) => {
-    if (board[index] != "") {
-      board.splice(index, 1, move);
-    }
-  };
-
-  return { getBoard, addMoveToBoard };
 })();
-
-function controlGame() {
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  let winner;
-  let gameOver = false;
-
-  const getIndexes = (board) => {
-    let xIndexes = [];
-    let oIndexes = [];
-
-    board.array.forEach((element, index) => {
-      if (element == "x") {
-        xIndexes.push(index);
-      } else if (element == "o") {
-        oIndexes.push(index);
-      }
-    });
-
-    return { xIndexes, oIndexes };
-  };
-
-  const xWinner = () => {
-    winner = "X's Win";
-    return { winner };
-  };
-
-  const oWinner = () => {
-    winner = "O's Win";
-    return { winner };
-  };
-
-  const identifyWinner = () => {
-    const { xIdx, oIdx } = getIndexes(gameboard.getBoard());
-
-    winningCombinations.forEach((element) => {
-      if (JSON.stringify(xIdx) == JSON.stringify(element)) {
-        xWinner();
-      } else if (JSON.stringify(oIdx) == JSON.stringify(element)) {
-        oWinner();
-      }
-    });
-  };
-
-  const checkGameOver = (board) => {
-    let notEmpty = [];
-    board.forEach((element) => {
-      if (element != "") {
-        notEmpty.pushEmpty;
-      }
-    });
-
-    if (notEmpty.length == 9) {
-      gameOver = true;
-    }
-  };
-
-  return { identifyWinner, checkGameOver };
-}
-
-function createPlayers() {
-  const getMoves = () => {
-    return { moves };
-  };
-
-  return { getMoves };
-}
