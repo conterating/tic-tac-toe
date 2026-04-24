518680102618fdbf9bdeeec844446f53a6292596
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2026-04-24 19:21:03 -0400

    added add move functionality

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const gameboard = (() => {
     },
   };
 })();
+
+function createPlayer(side) {
+  return {
+    side,
+  };
+}
+
+function makeMove(side, index) {
+  board = gameboard.getBoard();
+
+  return function addMoveToBoard() {
+    board.splice(index, 1, side);
+    console.log(board);
+  };
+}
+
+x3 = makeMove("x", 3);
+x3();
