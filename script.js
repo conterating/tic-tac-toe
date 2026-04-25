e8f1dd99f4ea4958479f32c05604724229d4735f
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2026-04-25 12:40:47 -0400

    got winning check functionality working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const gameboard = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
+const Gameboard = (() => {
+  const board = ["x", "x", "x", "", "", "", "", "", ""];
 
   return {
     getBoard() {
@@ -8,12 +8,16 @@ const gameboard = (() => {
   };
 })();
 
-function Player(side) {
-  board = gameboard.getBoard();
+function createPlayer(side) {
+  board = Gameboard.getBoard();
 
   function addMoveToBoard(index) {
-    board.splice(index, 1, side);
-    console.log(board);
+    if (board[index] == "") {
+      board.splice(index, 1, side);
+      console.log(board);
+    } else {
+      console.log("That spot is taken already!");
+    }
   }
 
   return {
@@ -21,5 +25,58 @@ function Player(side) {
   };
 }
 
-xUser = Player("x");
-xUser.addMoveToBoard(2);
+const gameController = (() => {
+  board = Gameboard.getBoard();
+
+  const winningIdxs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let xIdxs = [];
+  let oIdxs = [];
+  let winner;
+
+  function addIndices() {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == "x") {
+        xIdxs.push(i);
+      } else if (board[i] == "o") {
+        oIdxs.push(i);
+      }
+    }
+  }
+
+  function checkWinner() {
+    let counter = 0;
+
+    for (let arr in winningIdxs) {
+      if (JSON.stringify(winningIdxs[arr]) == JSON.stringify(xIdxs)) {
+        winner = "X's Won!!!!";
+        break;
+      } else if (JSON.stringify(winningIdxs[arr]) == JSON.stringify(oIdxs)) {
+        winner = "O's Won!!!!";
+        break;
+      }
+
+      counter += 1;
+
+      if (counter == 8) {
+        winner = "Draw....";
+      }
+    }
+
+    return winner;
+  }
+
+  return { addIndices, checkWinner };
+})();
+
+gameController.addIndices();
+console.log(gameController.checkWinner());
---
+++ test.js
@@ -0,0 +1,11 @@
+const nestedArray = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [9, 10, 11],
+  [12, 13, 14],
+];
+
+for (let arr in nestedArray) {
+  console.log(nestedArray[arr]);
+}
